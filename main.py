5635e490f265220da821b60c51f72fac21a05c5b
Suraj Singh <suraj1998@gmail.com>
2019-03-10 14:16:30 +0530

    searching all executable sections for gadgets

--- main.py
+++ main.py
@@ -33,22 +33,29 @@ if __name__ == "__main__":
 
     with open(vulnExecutable, 'rb') as fd:
         elffile = ELFFile(fd)
-        text_section = '.text'
-        code = elffile.get_section_by_name(text_section)
-        opcodes = code.data()
-        addr = code['sh_addr'] # section header address
-        print('Entry Point: '+ str(hex(elffile.header['e_entry'])))
-        EntryAddress = elffile.header['e_entry']
-        md = Cs(CS_ARCH_X86, CS_MODE_64)
-        instructions = md.disasm(opcodes,addr)
-        if instructions == 0:
-            print("Unable to disassemble executable")
-            exit(1)
-        # for i in instructions:
-        #     print("0x%x:\t%s\t%s" %(i.address, i.mnemonic,i.bytes))
-        print("Looking for c3s")
-        get_gadgets.GetAllGadgets(instructions, code.data(), EntryAddress, gadgetLength)
+        print("Searching all executable sections....")
+        for section in elffile.iter_sections():
+            curr_code = elffile.get_section_by_name(section.name)
+            # print("{1:08b}" % int(curr_code["sh_flags"]))
+            if(curr_code['sh_flags'] & 4): # only if the first bit of sh_flags is set, is the section executable and we can collect gadgets from here
+                print("Searching the " + section.name + " section")
+                section_name = section.name
+                code = elffile.get_section_by_name(section_name)
+                opcodes = code.data()
+                addr = code['sh_addr'] # section header address
+                # print('Entry Point: '+ str(hex(elffile.header['e_entry'])))
+                EntryAddress = elffile.header['e_entry']
+                md = Cs(CS_ARCH_X86, CS_MODE_64)
+                instructions = md.disasm(opcodes,addr)
+                if instructions == 0:
+                    print("Unable to disassemble executable")
+                    exit(1)
+                # for i in instructions:
+                #     print("0x%x:\t%s\t%s" %(i.address, i.mnemonic,i.bytes))
+                #print("Looking for c3s")
+                get_gadgets.GetAllGadgets(instructions, code.data(), EntryAddress, gadgetLength)
 
+    print("Gadgets that were found:")
     print_pretty.print_pretty(get_gadgets.allGadgets)    
     # For now, get all gadgets with just 1 Instruction in it(excluding ret).
     categorize.getLNGadgets(get_gadgets.allGadgets, 2)
